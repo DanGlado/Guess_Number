9e1e8f06ef39b5dc5f2a4c02a6448aaebea4cc12
202530 <dgladushenko@mail.ru>
2021-10-22 02:54:18 +0300

    Until Logical OnClick

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.*;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
 
+    int a = (int) ( Math.random() * 3 );
+    boolean lightsOn = false;
+    int v = Integer.parseInt(etInput.getText().toString());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView)findViewById(R.id.textView);
+        etInput = (EditText)findViewById(R.id.editText);
+        bControl = (Button)findViewById(R.id.button);
+    }
+    public void onClick(View v){
+
+        tvInfo.setText(getResources().getString(R.string.ahead));  // изменить значение текста в информационном поле
     }
 }
